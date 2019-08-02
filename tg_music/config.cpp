17afd5398a491a1db76cfac3c8d5df8140fca9fe
sentry-tg <53406925+sentry-tg@users.noreply.github.com>
2019-08-02 20:22:30 +0200

    Update config.cpp

--- tg_music/config.cpp
+++ tg_music/config.cpp
@@ -14,141 +14,141 @@ class CfgMusic
 	class tg_timebomb
 	{
 		name = "TG Time Bomb";
-		sound[] = {"\tg_music\tiberiansun\Timebomb.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\Timebomb.ogg",4.0,1.0};
 	};
 	class tg_pharotek
 	{
 		name = "TG Pharotek";
-		sound[] = {"\tg_music\tiberiansun\Pharotek.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\Pharotek.ogg",4.0,1.0};
 	};
 	class tg_lone_trooper
 	{
 		name = "TG Lone Trooper";
-		sound[] = {"\tg_music\tiberiansun\LoneTrooper.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\LoneTrooper.ogg",4.0,1.0};
 	};
 	class tg_scouting
 	{
 		name = "TG Scouting";
-		sound[] = {"\tg_music\tiberiansun\Scouting.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\Scouting.ogg",4.0,1.0};
 	};
 	class tg_infrared
 	{
 		name = "TG Infrared";
-		sound[] = {"\tg_music\tiberiansun\Infrared.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\Infrared.ogg",4.0,1.0};
 	};
 	class tg_flurry
 	{
 		name = "TG Flurry";
-		sound[] = {"\tg_music\tiberiansun\Flurry.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\Flurry.ogg",4.0,1.0};
 	};
 	class tg_mutants
 	{
 		name = "TG Mutants";
-		sound[] = {"\tg_music\tiberiansun\Mutants.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\Mutants.ogg",4.0,1.0};
 	};
 	class tg_gloom
 	{
 		name = "TG Gloom";
-		sound[] = {"\tg_music\tiberiansun\Gloom.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\Gloom.ogg",4.0,1.0};
 	};
 	class tg_heroism
 	{
 		name = "TG Heroism";
-		sound[] = {"\tg_music\tiberiansun\Heroism.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\Heroism.ogg",4.0,1.0};
 	};
 	class tg_approach
 	{
 		name = "TG Approach";
-		sound[] = {"\tg_music\tiberiansun\Approach.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\Approach.ogg",4.0,1.0};
 	};
 	class tg_dusk_hour
 	{
 		name = "TG Dusk Hour";
-		sound[] = {"\tg_music\tiberiansun\DuskHour.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\DuskHour.ogg",4.0,1.0};
 	};
 	class tg_the_defense
 	{
 		name = "TG The Defense";
-		sound[] = {"\tg_music\tiberiansun\TheDefense.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\TheDefense.ogg",4.0,1.0};
 	};
 	class tg_mad_rap
 	{
 		name = "TG Mad Rap";
-		sound[] = {"\tg_music\tiberiansun\MadRap.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\MadRap.ogg",4.0,1.0};
 	};
 	class tg_valves
 	{
 		name = "TG Valves";
-		sound[] = {"\tg_music\tiberiansun\Valves.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\Valves.ogg",4.0,1.0};
 	};
 	class tg_what_lurks
 	{
 		name = "TG What Lurks";
-		sound[] = {"\tg_music\tiberiansun\WhatLurks.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\WhatLurks.ogg",4.0,1.0};
 	};
 	class tg_score
 	{
 		name = "TG Score";
-		sound[] = {"\tg_music\tiberiansun\Score.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\Score.ogg",4.0,1.0};
 	};
 	class tg_ion_storm
 	{
 		name = "TG Ion Storm";
-		sound[] = {"\tg_music\tiberiansun\IonStorm.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\IonStorm.ogg",4.0,1.0};
 	};
 	class tg_nod_crush
 	{
 		name = "TG NOD Crush";
-		sound[] = {"\tg_music\tiberiansun\NodCrush.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\NodCrush.ogg",4.0,1.0};
 	};
 	class tg_red_sky
 	{
 		name = "TG Red Sky";
-		sound[] = {"\tg_music\tiberiansun\RedSky.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\RedSky.ogg",4.0,1.0};
 	};
 	class tg_tiberian_sun_map_theme
 	{
 		name = "TG Tiberian Sun Map Theme";
-		sound[] = {"\tg_music\tiberiansun\TiberianSunMapTheme.ogg",4.0,1.0};
+		sound[] = {"\tg_music\TiberianSun\TiberianSunMapTheme.ogg",4.0,1.0};
 	};
 	class tg_elusive
 	{
 		name = "TG Elusive";
-		sound[] = {"\tg_music\firestorm\Elusive.ogg",4.0,1.0};
+		sound[] = {"\tg_music\FireStorm\Elusive.ogg",4.0,1.0};
 	};
 	class tg_hacker
 	{
 		name = "TG Hacker";
-		sound[] = {"\tg_music\firestorm\Hacker.ogg",4.0,1.0};
+		sound[] = {"\tg_music\FireStorm\Hacker.ogg",4.0,1.0};
 	};
 	class tg_infiltration
 	{
 		name = "TG Infiltration";
-		sound[] = {"\tg_music\firestorm\Infiltration.ogg",4.0,1.0};
+		sound[] = {"\tg_music\FireStorm\Infiltration.ogg",4.0,1.0};
 	};
 	class tg_link_up
 	{
 		name = "TG Link Up";
-		sound[] = {"\tg_music\firestorm\LinkUp.ogg",4.0,1.0};
+		sound[] = {"\tg_music\FireStorm\LinkUp.ogg",4.0,1.0};
 	};
 	class tg_killing_machine
 	{
 		name = "TG Killing Machine";
-		sound[] = {"\tg_music\firestorm\KillingMachine.ogg",4.0,1.0};
+		sound[] = {"\tg_music\FireStorm\KillingMachine.ogg",4.0,1.0};
 	};
 	class tg_rain_in_the_night
 	{
 		name = "TG Rain in the Night";
-		sound[] = {"\tg_music\firestorm\RainintheNight.ogg",4.0,1.0};
+		sound[] = {"\tg_music\FireStorm\RainintheNight.ogg",4.0,1.0};
 	};
 	class tg_slave_to_the_system
 	{
 		name = "TG Slave to the System";
-		sound[] = {"\tg_music\firestorm\SlavetotheSystem.ogg",4.0,1.0};
+		sound[] = {"\tg_music\FireStorm\SlavetotheSystem.ogg",4.0,1.0};
 	};
 	class tg_deploy_machines
 	{
 		name = "TG Deploy Machines";
-		sound[] = {"\tg_music\firestorm\DeployMachines.ogg",4.0,1.0};
+		sound[] = {"\tg_music\FireStorm\DeployMachines.ogg",4.0,1.0};
 	};
 };
