2c151384552a6456ee34bced0fc53fec9dc977bb
kaydol <volyaandrazum@mail.ru>
2019-08-03 04:49:15 +1000

    Applied class naming convention

--- tg_music/config.cpp
+++ tg_music/config.cpp
@@ -11,142 +11,142 @@ class CfgPatches
 };
 class CfgMusic
 {
-	class tg_timebomb
+	class TG_TimeBomb
 	{
 		name = "TG Time Bomb";
 		sound[] = {"\tg_music\TiberianSun\Timebomb.ogg",4.0,1.0};
 	};
-	class tg_pharotek
+	class TG_Pharotek
 	{
 		name = "TG Pharotek";
 		sound[] = {"\tg_music\TiberianSun\Pharotek.ogg",4.0,1.0};
 	};
-	class tg_lone_trooper
+	class TG_LoneTrooper
 	{
 		name = "TG Lone Trooper";
 		sound[] = {"\tg_music\TiberianSun\LoneTrooper.ogg",4.0,1.0};
 	};
-	class tg_scouting
+	class TG_Scouting
 	{
 		name = "TG Scouting";
 		sound[] = {"\tg_music\TiberianSun\Scouting.ogg",4.0,1.0};
 	};
-	class tg_infrared
+	class TG_Infrared
 	{
 		name = "TG Infrared";
 		sound[] = {"\tg_music\TiberianSun\Infrared.ogg",4.0,1.0};
 	};
-	class tg_flurry
+	class TG_Flurry
 	{
 		name = "TG Flurry";
 		sound[] = {"\tg_music\TiberianSun\Flurry.ogg",4.0,1.0};
 	};
-	class tg_mutants
+	class TG_Mutants
 	{
 		name = "TG Mutants";
 		sound[] = {"\tg_music\TiberianSun\Mutants.ogg",4.0,1.0};
 	};
-	class tg_gloom
+	class TG_Gloom
 	{
 		name = "TG Gloom";
 		sound[] = {"\tg_music\TiberianSun\Gloom.ogg",4.0,1.0};
 	};
-	class tg_heroism
+	class TG_Heroism
 	{
 		name = "TG Heroism";
 		sound[] = {"\tg_music\TiberianSun\Heroism.ogg",4.0,1.0};
 	};
-	class tg_approach
+	class TG_Approach
 	{
 		name = "TG Approach";
 		sound[] = {"\tg_music\TiberianSun\Approach.ogg",4.0,1.0};
 	};
-	class tg_dusk_hour
+	class TG_DuskHour
 	{
 		name = "TG Dusk Hour";
 		sound[] = {"\tg_music\TiberianSun\DuskHour.ogg",4.0,1.0};
 	};
-	class tg_the_defense
+	class TG_TheDefense
 	{
 		name = "TG The Defense";
 		sound[] = {"\tg_music\TiberianSun\TheDefense.ogg",4.0,1.0};
 	};
-	class tg_mad_rap
+	class TG_MadRap
 	{
 		name = "TG Mad Rap";
 		sound[] = {"\tg_music\TiberianSun\MadRap.ogg",4.0,1.0};
 	};
-	class tg_valves
+	class TG_Valves
 	{
 		name = "TG Valves";
 		sound[] = {"\tg_music\TiberianSun\Valves.ogg",4.0,1.0};
 	};
-	class tg_what_lurks
+	class TG_WhatLurks
 	{
 		name = "TG What Lurks";
 		sound[] = {"\tg_music\TiberianSun\WhatLurks.ogg",4.0,1.0};
 	};
-	class tg_score
+	class TG_Score
 	{
 		name = "TG Score";
 		sound[] = {"\tg_music\TiberianSun\Score.ogg",4.0,1.0};
 	};
-	class tg_ion_storm
+	class TG_IonStorm
 	{
 		name = "TG Ion Storm";
 		sound[] = {"\tg_music\TiberianSun\IonStorm.ogg",4.0,1.0};
 	};
-	class tg_nod_crush
+	class TG_NodCrush
 	{
 		name = "TG NOD Crush";
 		sound[] = {"\tg_music\TiberianSun\NodCrush.ogg",4.0,1.0};
 	};
-	class tg_red_sky
+	class TG_RedSky
 	{
 		name = "TG Red Sky";
 		sound[] = {"\tg_music\TiberianSun\RedSky.ogg",4.0,1.0};
 	};
-	class tg_tiberian_sun_map_theme
+	class TG_TiberianSun_MapTheme
 	{
 		name = "TG Tiberian Sun Map Theme";
 		sound[] = {"\tg_music\TiberianSun\TiberianSunMapTheme.ogg",4.0,1.0};
 	};
-	class tg_elusive
+	class TG_Elusive
 	{
 		name = "TG Elusive";
 		sound[] = {"\tg_music\FireStorm\Elusive.ogg",4.0,1.0};
 	};
-	class tg_hacker
+	class TG_Hacker
 	{
 		name = "TG Hacker";
 		sound[] = {"\tg_music\FireStorm\Hacker.ogg",4.0,1.0};
 	};
-	class tg_infiltration
+	class TG_Infiltration
 	{
 		name = "TG Infiltration";
 		sound[] = {"\tg_music\FireStorm\Infiltration.ogg",4.0,1.0};
 	};
-	class tg_link_up
+	class TG_LinkUp
 	{
 		name = "TG Link Up";
 		sound[] = {"\tg_music\FireStorm\LinkUp.ogg",4.0,1.0};
 	};
-	class tg_killing_machine
+	class TG_KillingMachine
 	{
 		name = "TG Killing Machine";
 		sound[] = {"\tg_music\FireStorm\KillingMachine.ogg",4.0,1.0};
 	};
-	class tg_rain_in_the_night
+	class TG_RainInTheNight
 	{
 		name = "TG Rain in the Night";
 		sound[] = {"\tg_music\FireStorm\RainintheNight.ogg",4.0,1.0};
 	};
-	class tg_slave_to_the_system
+	class TG_SlaveToTheSystem
 	{
 		name = "TG Slave to the System";
 		sound[] = {"\tg_music\FireStorm\SlavetotheSystem.ogg",4.0,1.0};
 	};
-	class tg_deploy_machines
+	class TG_DeployMachines
 	{
 		name = "TG Deploy Machines";
 		sound[] = {"\tg_music\FireStorm\DeployMachines.ogg",4.0,1.0};
